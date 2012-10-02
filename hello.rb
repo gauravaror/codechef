629c6e2bb97cb03a6b7d29c7fb6bdc695129bfb3
Gaurav Arora <gauravarora.daiict@gmail.com>
2012-10-02 15:44:45 +0530

    remo   file# Please enter the commit message for your changes. Lines starting

--- hello.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ruby
-#is theis
-puts "what is your name ?"
-
-name = gets
-puts "Hello  " + name.chomp + ",How are you ?"
--- lifeuniverse.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-a = 0
-while a != 42 do
-	a =	gets
-	if Integer(a) == 42
-     break
-	end
-	puts a
-end
