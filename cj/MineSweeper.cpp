599c43b263790bb81415a3a5e8f3746084eab97a
samuelharden <gauravarora.daiict@gmail.com>
2014-04-13 21:31:05 +0000

    Google code jam Qualification round codes

---
+++ cj/MineSweeper.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h> 
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+	int notest;
+	cin >> notest;
+	//cout<<notest;
+	
+	for (int i=0;i<notest;i++) {
+
+	}
+}
