92a9ef33e063da3db8c7741e8fed7de057e6919a
samuelharden <gauravarora.daiict@gmail.com>
2014-05-05 20:56:28 +0000

    minor changer for streaming video on youtube(not yet functional)

--- chromeextension/changeTitle.js
+++ chromeextension/changeTitle.js
@@ -1,2 +1,41 @@
+var handelerclick = function() {
+document.title = "This is Youtube clicked";
+alert(document.title);
+}
 
-document.title = "This is Youtube";
+var handelerplay = function() {
+alert("pased");
+}
+
+
+var handelerchange = function() {
+document.title = "This is Youtube change";
+alert(document.title);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    alert("sdfsd"+getQueryVariable("v"));
+    var vid = document.getElementsByTagName('video')[0];
+    vid.src = "https://127.0.0.1:8000/video.mp4";
+    vid.addEventListener('click', function(){
+    var vid = document.getElementsByTagName('video')[0];
+    alert("sdfsddsfdsf : "+vid.click + "  "+ vid);
+    alert("sdfsd");
+    var vid = document.getElementsByTagName('video')[0];
+    vid.addEventListerner('change',handelerplay);
+    var vid = document.getElementsByTagName('video')[0];
+    vid.addEventListerner('change',handelerchange);
+});
+});
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
