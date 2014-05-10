810e36519b6022d49027359095c853708dc00751
samuelharden <gauravarora.daiict@gmail.com>
2014-05-10 15:07:57 +0000

    Added a check to see if video there then only load our video else, let youtube play and download in background

--- chromeextension/changeTitle.js
+++ chromeextension/changeTitle.js
@@ -1,3 +1,7 @@
+var stream;
+var videoavailable = false;
+
+
 var handelerclick = function() {
 document.title = "This is Youtube clicked";
 alert(document.title);
@@ -17,20 +21,42 @@ document.addEventListener('DOMContentLoaded', function () {
 //    alert("sdfsd"+getQueryVariable("v"));
     var vid = document.getElementsByTagName('video')[0];
     var videoid = getQueryVariable("v");
-//    vid.src = "https://127.0.0.1:8000/"+videoid+".mp4";
-  //  alert("sdfsd");
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4) {
+                if (xhr.status==200) {
+                    videoavailable= true;
+                }
+         }
+    }
+    xhr.open("GET", "http://127.0.0.1:8001/"+videoid+".mp4", true);
+    xhr.send(null);
     vid.addEventListener('loadstart',loadstarthandaler);
+    vid.addEventListener('error',errorhandaler);
+  //  vid.addEventListener('abort',errorhandaler);
 });
 
 function loadstarthandaler () {
     var vid = document.getElementsByTagName('video')[0];
     var videoid = getQueryVariable("v");
-    var stream = vid.src;
-    vid.src = "https://127.0.0.1:8000/"+videoid+".mp4";
-    vid.autoplay = true
-    vid.removeEventListener('loadstart',loadstarthandaler);
-
+//    alert(xhr.status);
+    alert(videoavailable);
+    if (videoavailable) {
+        stream = vid.src;
+        vid.src = "https://127.0.0.1:8000/"+videoid+".mp4";
+        vid.autoplay = true
+        vid.removeEventListener('loadstart',loadstarthandaler);
     }
+
+}
+
+
+function errorhandaler() {
+    var vid = document.getElementsByTagName('video')[0];
+    vid.src = stream;
+    vid.autoplay = true
+}
+
 function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split('&');
