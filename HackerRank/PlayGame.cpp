8ca08108f583e523b367844072695e5403f047d6
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-08-28 19:59:35 +0530

    done with skelton and thought of logic

--- HackerRank/PlayGame.cpp
+++ HackerRank/PlayGame.cpp
@@ -20,6 +20,7 @@ int main() {
 		int bricks;
 		cin>>bricks;
 		long long int * storebricks = (long long int *)malloc(bricks*sizeof(long long int));
+		unsigned long long int optimalMove = (unsigned long long int *)malloc(bricks*sizeof(unsigned long long int));
 		if(debug) 
 		cout<<"bricks for this testcase:"<<bricks<<endl;
 		for(int j = 0;j < bricks; j++){
@@ -31,13 +32,55 @@ int main() {
 			}
 			cout<<endl;
 		}
-		calculateMaxScore(storebricks,0,bricks-1);
+		findOptiomalMoves(storebricks,optimalMove , 0,bricks-1);
+		calculateMaxScore(storebricks,optimalMove , 0,bricks-1);
 
 	}
 
 }
 
-long long int calculateMaxScore(long long int * bricks,int start,int end) {
+long long int findOptiomalMoves (long long int * bricks,unsigned long long int optimalMove, int start,int end) {
+
+	for( int i=end ; i< start; i--) {
+		long long int picked1 = pick1(storebricks,optimalMove , i,end);
+		long long int picked2 = pick2(storebricks,optimalMove , i,end);
+		long long int picked3 = pick3(storebricks,optimalMove , i,end);
+		if( picked1 > picked3) {
+			if(picked1 >picked2) {
+
+			} else if(picked1 >picked2) {
+
+			} else if(picked1 == picked2 ) {
+
+			}
+		} else if (picked1 < picked3) {
+			if(picked3 > picked2) {
+
+			} else if(picked3 >picked2) {
+
+			} else if(picked3 == picked2 ) {
+
+			}
+
+		}
+	}
 
 
+}
+
+long long int calculateMaxScore (long long int * bricks,unsigned long long int optimalMove, int start,int end) {
+
+
+}
+
+long long int pick1 (long long int * bricks,unsigned long long int optimalMove, int current,int end) {
+
+}
+
+long long int pick2 (long long int * bricks,unsigned long long int optimalMove, int current,int end) {
+	
+}
+
+long long int pick3 (long long int * bricks,unsigned long long int optimalMove, int current,int end) {
+	
 }
