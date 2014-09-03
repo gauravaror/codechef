e2e02b935f788570d49aebd86d44733f7a5723d1
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-09-03 19:32:12 +0530

    sample creation for issue on angular

---
+++ angular/testlocationchangestart/app.js
@@ -0,0 +1,49 @@
+var app = angular.module('plunker', []);
+ 
+app.controller('ArticleDetailController', ['$scope', '$rootScope','$location', function ($scope, $rootScope,$location) {
+  $scope.name = 'World';
+  $scope.urll = $location.absUrl();
+  $scope.urlll = $location.protocol();
+  $scope.go = function() {
+      $rootScope.preventNavigation();
+
+    $location.path("/here");
+    //alert("lfd");
+  }
+}]);
+
+app.run(['$rootScope', '$location', function ($rootScope, $location) {
+
+  var _preventNavigation = false;
+  var _preventNavigationUrl = null;
+
+  $rootScope.allowNavigation = function() {
+    _preventNavigation = false;
+  };
+
+  $rootScope.preventNavigation = function() {
+    _preventNavigation = true;
+    _preventNavigationUrl = $location.absUrl();
+  }
+
+  $rootScope.$on('$locationChangeStart', function (event, newUrl, oldUrl) {
+    // Allow navigation if our old url wasn't where we prevented navigation from
+   /* if (_preventNavigationUrl != oldUrl || _preventNavigationUrl === null) {
+      $rootScope.allowNavigation();
+      return;
+    }*/
+
+    if (_preventNavigation && !confirm("You have unsaved changes, do you want to continue?")) {
+      alert($location.absUrl()+" s "+$location.url());
+      event.preventDefault();
+    }
+    else {
+       alert($location.absUrl()+" d "+$location.url());
+      $rootScope.allowNavigation();
+    }
+  });
+}]);
+
+
+
+
